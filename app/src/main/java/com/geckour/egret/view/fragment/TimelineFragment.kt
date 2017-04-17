92849deda9c82db30cdf34869b80be763597d12e
geckour <ishida@seesaa.co.jp>
2017-04-17 20:06:11 +0900

    Timeline表示用のレイアウトファイルの追加、ダミー画像の追加

---
+++ app/src/main/java/com/geckour/egret/view/fragment/TimelineFragment.kt
@@ -0,0 +1,7 @@
+package com.geckour.egret.view.fragment
+
+import com.trello.rxlifecycle2.components.support.RxFragment
+
+class TimelineFragment: RxFragment() { // TODO: Timelineを取得、RecyclerViewを使って表示
+
+}
