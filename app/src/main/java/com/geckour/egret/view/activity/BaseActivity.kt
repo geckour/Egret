7aa65de125649ed6d1bf1d4c3da437f18d2ce370
geckour <geckour@gmail.com>
2017-04-29 09:50:16 +0900

    ソフトキーボードが適切なタイミングで閉じられるように

--- app/src/main/java/com/geckour/egret/view/activity/BaseActivity.kt
+++ app/src/main/java/com/geckour/egret/view/activity/BaseActivity.kt
@@ -9,8 +9,8 @@ import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 open class BaseActivity: RxAppCompatActivity() {
     fun showSoftKeyBoardOnFocusEditText(et: EditText) {
         et.setOnFocusChangeListener { view, hasFocus ->
-            if (hasFocus) (view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).showSoftInput(view, InputMethodManager.SHOW_FORCED)
-            else (view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(view.windowToken, 0)
+            if (hasFocus) (view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+            else (view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromInputMethod(view.applicationWindowToken, 0)
         }
         et.requestFocusFromTouch()
         et.requestFocus()
